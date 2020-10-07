2b8e956b0c00f7eaedf59f4b2afb17b47065a143
KaneishaR <63017525+KaneishaR@users.noreply.github.com>
2020-10-07 11:13:31 -0500

    Add files via upload

---
+++ src/main/java/com/dandk/FamilyReunionAPI/FamilyReunionApiApplication.java
@@ -0,0 +1,13 @@
+package com.dandk.FamilyReunionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyReunionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyReunionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dandk/FamilyReunionAPI/FamilyReunionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dandk.FamilyReunionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyReunionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
